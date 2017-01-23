2be9ad13c2cf52d519e5b6c3f083e293159ad04a
Maciej Gierok <mgierok@gmail.com>
2017-01-23 10:49:29 +0100

    new: quotes db table and instruction to import historical data

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # monujo
 The investment portfolio
+
+##HowTo Import Quotes?
+
+ 1. Go to http://stooq.com/db/h/
+ 2. Download daily quotes in ASCII format
+ 3. Unzip downloaded file
+ 4. Run below for each requested ticker name:
+```
+ticker=[ticker]; find . -name "${ticker,,}.txt" -exec sed "s/^/${ticker^^},/g" {} \; | psql -h [hostname] -U [username] -d [dbname] -c "COPY public.quotes FROM STDIN (FORMAT 'csv', DELIMITER ',', HEADER)"
+```
---
+++ db/schema.dump.sql
@@ -0,0 +1,70 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.4.1
+-- Dumped by pg_dump version 9.5.1
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET search_path = "public", pg_catalog;
+
+--
+-- Name: quotes_before_insert(); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION "quotes_before_insert"() RETURNS "trigger"
+    LANGUAGE "plpgsql"
+    AS $$
+                    BEGIN
+                        DELETE FROM quotes where "ticker" = NEW."ticker" AND "date" = NEW."date";
+                        RETURN NEW;
+                    END;
+                    $$;
+
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: quotes; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE "quotes" (
+    "ticker" character(8) NOT NULL,
+    "date" "date" NOT NULL,
+    "open" real,
+    "high" real,
+    "low" real,
+    "close" real,
+    "volume" bigint,
+    "openint" smallint
+);
+
+
+--
+-- Name: quotes_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY "quotes"
+    ADD CONSTRAINT "quotes_pkey" PRIMARY KEY ("ticker", "date");
+
+
+--
+-- Name: quotes_before_insert_trigger; Type: TRIGGER; Schema: public; Owner: -
+--
+
+CREATE TRIGGER "quotes_before_insert_trigger" BEFORE INSERT ON "quotes" FOR EACH ROW EXECUTE PROCEDURE "quotes_before_insert"();
+
+
+--
+-- PostgreSQL database dump complete
+--
+
