9eaba86290fab5be883f8777fb015f283cbdd913
Maciej Gierok <mgierok@gmail.com>
2017-04-02 00:01:04 +0200

    rename subpackage models to entities

--- repository/portfolios_summary.go
+++ repository/portfolios_summary.go
@@ -2,11 +2,11 @@ package repository
 
 import (
 	"github.com/jmoiron/sqlx"
-	"github.com/mgierok/monujo/repository/models"
+	"github.com/mgierok/monujo/repository/entities"
 )
 
-func GetAllPortfoliosSummary(db *sqlx.DB) ([]models.PortfolioSummary, error) {
-	portfoliosSummary := []models.PortfolioSummary{}
+func GetAllPortfoliosSummary(db *sqlx.DB) ([]entities.PortfolioSummary, error) {
+	portfoliosSummary := []entities.PortfolioSummary{}
 	err := db.Select(&portfoliosSummary, "SELECT portfolio_id, name, currency, cache_value, outgoings, incomings, gain_of_sold_shares, commision, tax, gain_of_owned_shares, estimated_gain, estimated_gain_costs_inc FROM portfolios_summary")
 	return portfoliosSummary, err
 }
