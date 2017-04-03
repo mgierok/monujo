e3dffe278c1560e161537ad9a37de7445610e9fa
Maciej Gierok <mgierok@gmail.com>
2017-04-03 23:07:13 +0200

    fetch portfolios

--- repository/portfolios.go
+++ repository/portfolios.go
@@ -9,3 +9,9 @@ func PortfoliosExt() (entities.PortfoliosExt, error) {
 	err := Db().Select(&portfolios, "SELECT portfolio_id, name, currency, cache_value, outgoings, incomings, gain_of_sold_shares, commision, tax, gain_of_owned_shares, estimated_gain, estimated_gain_costs_inc FROM portfolios_ext")
 	return portfolios, err
 }
+
+func Portfolios() (entities.Portfolios, error) {
+	portfolios := entities.Portfolios{}
+	err := Db().Select(&portfolios, "SELECT portfolio_id, name, currency FROM portfolios ORDER BY portfolio_id ASC")
+	return portfolios, err
+}
