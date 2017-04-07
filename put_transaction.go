71dfc523662bc03ce05afbbec65fc6b819eba69d
Maciej Gierok <mgierok@gmail.com>
2017-04-07 22:40:10 +0200

    refactor put transaction action

--- put_transaction.go
+++ put_transaction.go
@@ -13,16 +13,16 @@ import (
 func PutTransaction() {
 
 	var t entities.Transaction
-	t.PortfolioId = choosePortfolio()
-	t.Date = provideDateOfTransaction()
-	t.Ticker = provideTicker()
-	t.Price = providePrice()
-	t.TransactionOperationType = chooseTypeOfTransaction()
-	t.Currency = chooseCurrency()
-	t.Shares = provideNumberOfShares()
-	t.Commision = provideCommision()
-	t.ExchangeRate = provideExchangeRate()
-	t.Tax = provideTax()
+	t.PortfolioId = portfolioId()
+	t.Date = date()
+	t.Ticker = ticker()
+	t.Price = price()
+	t.TransactionOperationType = transactionOperationType()
+	t.Currency = currency()
+	t.Shares = shares()
+	t.Commision = commision()
+	t.ExchangeRate = exchangeRate()
+	t.Tax = tax()
 
 	summary := [][]string{
 		[]string{"Portfolio ID", strconv.FormatInt(t.PortfolioId, 10)},
@@ -44,7 +44,7 @@ func PutTransaction() {
 	fmt.Printf("Transaction has been recorded with an ID: %d\n", transactionId)
 }
 
-func choosePortfolio() int64 {
+func portfolioId() int64 {
 	fmt.Println("Choose portfolio")
 	fmt.Println("")
 
@@ -56,38 +56,89 @@ func choosePortfolio() int64 {
 		"Portfolio Name",
 	}
 
-	var portfoliosDict = make(map[int64]string)
+	var dict = make(map[int64]string)
 	var data [][]string
 	for _, p := range portfolios {
 		data = append(data, []string{p.PortfolioId.String, p.Name.String})
 		portfolioId, _ := strconv.ParseInt(p.PortfolioId.String, 10, 64)
-		portfoliosDict[portfolioId] = p.Name.String
+		dict[portfolioId] = p.Name.String
 	}
 
 	DrawTable(header, data)
 	fmt.Println("")
 
-	var portfolioIdString string
+	var input string
 	fmt.Print("Portfolio ID: ")
-	fmt.Scanln(&portfolioIdString)
+	fmt.Scanln(&input)
 
-	portfolioId, err := strconv.ParseInt(portfolioIdString, 10, 64)
+	p, err := strconv.ParseInt(input, 10, 64)
 
 	if nil != err {
-		fmt.Printf("\n%sd is not a valid portfolio ID\n\n", portfolioIdString)
-		return choosePortfolio()
+		fmt.Printf("\n%sd is not a valid portfolio ID\n\n", input)
+		return portfolioId()
 	}
 
-	_, exists := portfoliosDict[portfolioId]
+	_, exists := dict[p]
 	if exists {
-		return portfolioId
+		return p
 	} else {
-		fmt.Printf("\n%d is not a valid portfolio ID\n\n", portfolioId)
-		return choosePortfolio()
+		fmt.Printf("\n%d is not a valid portfolio ID\n\n", p)
+		return portfolioId()
 	}
 }
 
-func chooseTypeOfTransaction() string {
+func date() string {
+	const layout = "2006-01-02"
+	var now = time.Now().Format(layout)
+	var d string
+
+	fmt.Printf("Date (default: %q): ", now)
+	fmt.Scanln(&d)
+
+	d = strings.Trim(d, " ")
+	if d == "" {
+		d = now
+	} else {
+		_, err := time.Parse(layout, d)
+		if err != nil {
+			fmt.Println(err)
+			fmt.Printf("\n%q is not a valid date format\n\n", d)
+			d = date()
+		}
+	}
+
+	return d
+}
+
+func ticker() string {
+	fmt.Print("Ticker: ")
+	var t string
+	fmt.Scanln(&t)
+
+	t = strings.Trim(t, " ")
+	if t == "" {
+		return ticker()
+	}
+
+	return strings.ToUpper(t)
+}
+
+func price() float64 {
+	fmt.Print("Price: ")
+	var input string
+	fmt.Scanln(&input)
+
+	p, err := strconv.ParseFloat(input, 64)
+
+	if err != nil {
+		fmt.Printf("\n%s is not a valid price value\n\n", input)
+		return price()
+	}
+
+	return p
+}
+
+func transactionOperationType() string {
 	fmt.Println("Choose type of transaction")
 	fmt.Println("")
 
@@ -98,151 +149,30 @@ func chooseTypeOfTransaction() string {
 		"Transaction Type",
 	}
 
-	var operationTypesDict = make(map[string]string)
+	var dict = make(map[string]string)
 	var data [][]string
 	for _, ot := range operationTypes {
-		operationTypesDict[ot.Type] = ot.Type
+		dict[ot.Type] = ot.Type
 		data = append(data, []string{ot.Type})
 	}
 
 	DrawTable(header, data)
 	fmt.Println("")
 
-	var operationType string
+	var ot string
 	fmt.Print("Transaction type: ")
-	fmt.Scanln(&operationType)
+	fmt.Scanln(&ot)
 
-	_, exists := operationTypesDict[operationType]
+	_, exists := dict[ot]
 	if exists {
-		return operationType
+		return ot
 	} else {
-		fmt.Printf("\n%s is not a valid transaction type\n\n", operationType)
-		return chooseTypeOfTransaction()
+		fmt.Printf("\n%s is not a valid transaction type\n\n", ot)
+		return transactionOperationType()
 	}
 }
 
-func provideDateOfTransaction() string {
-	const layout = "2006-01-02"
-	var now = time.Now().Format(layout)
-	var date string
-
-	fmt.Printf("Date (default: %q): ", now)
-	fmt.Scanln(&date)
-
-	if date == "" {
-		date = now
-	} else {
-		_, err := time.Parse(layout, date)
-		if err != nil {
-			fmt.Println(err)
-			fmt.Printf("\n%q is not a valid date format\n\n", date)
-			date = provideDateOfTransaction()
-		}
-	}
-
-	return date
-}
-
-func provideTicker() string {
-	fmt.Print("Ticker: ")
-	var ticker string
-	fmt.Scanln(&ticker)
-
-	if ticker == "" {
-		return provideTicker()
-	}
-
-	return strings.ToUpper(ticker)
-}
-
-func providePrice() float64 {
-	fmt.Print("Price: ")
-	var price string
-	fmt.Scanln(&price)
-
-	floatPrice, err := strconv.ParseFloat(price, 64)
-
-	if err != nil {
-		fmt.Printf("\n%s is not a valid price value\n\n", price)
-		return providePrice()
-	}
-
-	return floatPrice
-}
-
-func provideNumberOfShares() float64 {
-	fmt.Print("Number of shares: ")
-	var shares string
-	fmt.Scanln(&shares)
-
-	floatShares, err := strconv.ParseFloat(shares, 64)
-
-	if err != nil {
-		fmt.Printf("\n%s is not a valid share number value\n\n", shares)
-		return provideNumberOfShares()
-	}
-
-	return floatShares
-}
-
-func provideExchangeRate() float64 {
-	fmt.Print("Exchange rate (default: 1):")
-	var exchangeRate string
-	fmt.Scanln(&exchangeRate)
-
-	floatExchangeRate, err := strconv.ParseFloat(exchangeRate, 64)
-
-	if exchangeRate == "" {
-		floatExchangeRate = 1
-	} else {
-		if err != nil {
-			fmt.Printf("\n%s is not a valid exchange rate value\n\n", exchangeRate)
-			floatExchangeRate = provideExchangeRate()
-		}
-	}
-
-	return floatExchangeRate
-}
-
-func provideCommision() float64 {
-	fmt.Print("Commision (default: 0): ")
-	var commision string
-	fmt.Scanln(&commision)
-
-	floatCommision, err := strconv.ParseFloat(commision, 64)
-
-	if commision == "" {
-		floatCommision = 0
-	} else {
-		if err != nil {
-			fmt.Printf("\n%s is not a valid commision value\n\n", commision)
-			floatCommision = provideCommision()
-		}
-	}
-
-	return floatCommision
-}
-
-func provideTax() float64 {
-	fmt.Print("Tax (default: 0): ")
-	var tax string
-	fmt.Scanln(&tax)
-
-	floatTax, err := strconv.ParseFloat(tax, 64)
-
-	if tax == "" {
-		floatTax = 0
-	} else {
-		if err != nil {
-			fmt.Printf("\n%s is not a valid tax value\n\n", tax)
-			floatTax = provideTax()
-		}
-	}
-
-	return floatTax
-}
-
-func chooseCurrency() string {
+func currency() string {
 	fmt.Println("Choose currency")
 	fmt.Println("")
 
@@ -253,27 +183,99 @@ func chooseCurrency() string {
 		"Currency",
 	}
 
-	var currenciesDict = make(map[string]string)
+	var dict = make(map[string]string)
 	var data [][]string
 	for _, c := range currencies {
-		currenciesDict[c.Symbol] = c.Symbol
+		dict[c.Symbol] = c.Symbol
 		data = append(data, []string{c.Symbol})
 	}
 
 	DrawTable(header, data)
 	fmt.Println("")
 
-	var currency string
+	var c string
 	fmt.Print("Currency: ")
-	fmt.Scanln(&currency)
+	fmt.Scanln(&c)
 
-	currency = strings.ToUpper(currency)
+	c = strings.ToUpper(c)
 
-	_, exists := currenciesDict[currency]
+	_, exists := dict[c]
 	if exists {
-		return currency
+		return c
 	} else {
-		fmt.Printf("\n%s is not a valid currency\n\n", currency)
-		return chooseCurrency()
+		fmt.Printf("\n%s is not a valid currency\n\n", c)
+		return currency()
 	}
+}
+
+func shares() float64 {
+	fmt.Print("Number of shares: ")
+	var input string
+	fmt.Scanln(&input)
+
+	s, err := strconv.ParseFloat(input, 64)
+
+	if err != nil {
+		fmt.Printf("\n%s is not a valid share number value\n\n", input)
+		return shares()
+	}
+
+	return s
+}
+
+func exchangeRate() float64 {
+	fmt.Print("Exchange rate (default: 1):")
+	var input string
+	fmt.Scanln(&input)
+
+	er, err := strconv.ParseFloat(input, 64)
+
+	if input == "" {
+		er = 1
+	} else {
+		if err != nil {
+			fmt.Printf("\n%s is not a valid exchange rate value\n\n", input)
+			er = exchangeRate()
+		}
+	}
+
+	return er
+}
+
+func commision() float64 {
+	fmt.Print("Commision (default: 0): ")
+	var input string
+	fmt.Scanln(&input)
+
+	c, err := strconv.ParseFloat(input, 64)
+
+	if input == "" {
+		c = 0
+	} else {
+		if err != nil {
+			fmt.Printf("\n%s is not a valid commision value\n\n", input)
+			c = commision()
+		}
+	}
+
+	return c
+}
+
+func tax() float64 {
+	fmt.Print("Tax (default: 0): ")
+	var input string
+	fmt.Scanln(&input)
+
+	t, err := strconv.ParseFloat(input, 64)
+
+	if input == "" {
+		t = 0
+	} else {
+		if err != nil {
+			fmt.Printf("\n%s is not a valid tax value\n\n", input)
+			t = tax()
+		}
+	}
+
+	return t
 }
