60086085cc9e75191b51648042025dea094bff30
Maciej Gierok <maciej.gierok@performgroup.com>
2017-04-25 19:52:31 +0200

    allow to quit from main manu

--- monujo.go
+++ monujo.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/mgierok/monujo/repository"
 	"github.com/mgierok/monujo/console"
@@ -13,23 +14,27 @@ func main() {
 		[]interface{}{"1", "Summary"},
 		[]interface{}{"2", "Put transaction"},
 		[]interface{}{"3", "List transactions"},
+		[]interface{}{"Q", "Quit"},
 	}
 
 	console.DrawTable([]string{}, data)
 
-	var action int
+	var action string
 	fmt.Scanln(&action)
+	action = strings.ToUpper(action)
 	console.Clear()
 
 	db := GetDbConnection()
 	repository.SetDb(db)
 	defer db.Close()
 
-	if action == 1 {
+	if action == "1" {
 		Summary()
-	} else if action == 2 {
+	} else if action == "2" {
 		PutTransaction()
-	} else if action == 3 {
+	} else if action == "3" {
 		Transactions()
+	} else if action == "Q" {
+		return;
 	}
 }
