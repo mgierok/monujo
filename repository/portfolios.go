16b353ce4ff8cab3447b428c6918554bc2917f5b
Maciej Gierok <maciej.gierok@performgroup.com>
2017-10-27 11:11:42 +0200

    Order portfolio summary by ID

--- repository/portfolios.go
+++ repository/portfolios.go
@@ -7,7 +7,7 @@ import (
 
 func PortfoliosExt() (entity.PortfoliosExt, error) {
 	portfolios := entity.PortfoliosExt{}
-	err := db.Connection().Select(&portfolios, "SELECT portfolio_id, name, currency, cache_value, gain_of_sold_shares, commision, tax, gain_of_owned_shares, estimated_gain, estimated_gain_costs_inc, estimated_value, annual_balance, month_balance FROM portfolios_ext")
+	err := db.Connection().Select(&portfolios, "SELECT portfolio_id, name, currency, cache_value, gain_of_sold_shares, commision, tax, gain_of_owned_shares, estimated_gain, estimated_gain_costs_inc, estimated_value, annual_balance, month_balance FROM portfolios_ext ORDER BY portfolio_id")
 	return portfolios, err
 }
 
